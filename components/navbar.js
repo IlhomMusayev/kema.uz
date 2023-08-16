efa8a156a106646f72190118ded2b43a4989b0fa
Ilhom Musayev <ilhommusayev2001@gmail.com>
2023-08-16 21:12:03 +0500

    fixed

--- components/navbar.js
+++ components/navbar.js
@@ -33,7 +33,7 @@ export default function Navbar() {
                   <a className="flex items-center space-x-2 text-2xl font-medium text-indigo-500 dark:text-gray-100">
                     <span>
                       <img
-                        src="/img/logo.svg"
+                        src="/img/logo.png"
                         alt="N"
                         width="52"
                         height="52"
