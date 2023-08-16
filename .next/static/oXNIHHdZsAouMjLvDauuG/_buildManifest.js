c9cab6dde4b796b11eb0ccfd3c98814260f42e23
Ilhom Musayev <ilhommusayev2001@gmail.com>
2023-08-16 21:10:34 +0500

    fixed

--- .next/static/oXNIHHdZsAouMjLvDauuG/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/673-b23380b1282f546ff724.js","static/chunks/pages/index-b39baeea2afce87304d7.js"],"/_error":["static/chunks/pages/_error-82a806cd39f8ab3dc3ac.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
